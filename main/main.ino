e36bede6999e05c6db462114f72fe84dfad1c87e
Thuan Le <devicedev4@biotricity.com>
2022-03-12 22:23:54 +0700

    update: Set default LED

--- main/main.ino
+++ main/main.ino
@@ -15,32 +15,53 @@
 #include "bsp_io_11.h"
 
 /* Private defines ---------------------------------------------------- */
-#define NUM_LED           (300) // Total LED of on strip
+#define STRIP_MAX (7)
 
 /* Private enumerate/structure ---------------------------------------- */
+typedef struct
+{
+  uint8_t index;
+  uint8_t pin_out;
+  uint16_t num_led;
+  uint16_t brightness;
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+}
+led_strip_info_t;
+
 /* Private macros ----------------------------------------------------- */
 /* Public variables --------------------------------------------------- */
+#define INFO(_i, _io, _n, _bn, _r, _g, _b) [_i] = {.index = _i, .pin_out = _io, .num_led = _n, .brightness = _bn, .red = _r, .green = _g, .blue = _b}
+const led_strip_info_t LED_TABLE[] = 
+{
+   //     +=========================+=========+========+=====+=======+======+
+   //     |Index | Pinout           | Num LED | Bright | Red | Green | Blue |
+   //     +-------------------------+---------+--------+-----+-------+------+
+      INFO(0     , IO_LED_BUFFER_0  , 20      , 100    ,  255,    255,   255)
+     ,INFO(1     , IO_LED_BUFFER_1  , 20      , 100    ,  255,    255,   255)
+     ,INFO(2     , IO_LED_BUFFER_2  , 20      , 100    ,  255,    255,   255)
+     ,INFO(3     , IO_LED_BUFFER_3  , 20      , 70     ,  229,    153,   249)
+     ,INFO(4     , IO_LED_BUFFER_4  , 20      , 50     ,  204,    255,   255)
+     ,INFO(5     , IO_LED_BUFFER_5  , 20      , 30     ,   28,    110,   210)
+     ,INFO(6     , IO_LED_BUFFER_6  , 20      , 10     ,  130,      0,   186)
+   //     +======+==================+=========+========+=====+=======+======+
+};
+#undef INFO
+
 /* Private variables -------------------------------------------------- */
 Adafruit_NeoPixel strip[8] = { Adafruit_NeoPixel() };
 
 /* Function definitions ----------------------------------------------- */
 void setup()
 {
-  strip[0].setPin(IO_LED_BUFFER_0);
-  strip[1].setPin(IO_LED_BUFFER_1);
-  strip[2].setPin(IO_LED_BUFFER_2);
-  strip[3].setPin(IO_LED_BUFFER_3);
-  strip[4].setPin(IO_LED_BUFFER_4);
-  strip[5].setPin(IO_LED_BUFFER_5);
-  strip[6].setPin(IO_LED_BUFFER_6);
-  strip[7].setPin(IO_LED_BUFFER_7);
-
-  for (uint8_t i = 0; i < 8; i++)
+  for (uint8_t i = 0; i < STRIP_MAX; i++)
   {
-    strip[i].updateLength(NUM_LED);
+    strip[i].setPin(LED_TABLE[i].pin_out);
+    strip[i].updateLength(LED_TABLE[i].num_led);
     strip[i].updateType(NEO_GRB + NEO_KHZ800);
+    strip[i].setBrightness(LED_TABLE[i].brightness);
     strip[i].begin();
-    strip[i].setBrightness(30);
   }
 }
 
@@ -48,16 +69,15 @@ void loop()
 {
   uint16_t i;
 
-  for (uint8_t num_strip = 0; num_strip < 8; num_strip++)
+  for (uint8_t num_strip = 0; num_strip < STRIP_MAX; num_strip++)
   {
     for (i = 0; i < strip[0].numPixels(); i++)
     {
-      strip[num_strip].setPixelColor(i, 200, 250, 0, 0);
+      strip[num_strip].setPixelColor(i, LED_TABLE[num_strip].red, LED_TABLE[num_strip].green, LED_TABLE[num_strip].blue, 0);
     }
 
     strip[num_strip].show();
   }
-
 }
 
 /* Private function definitions --------------------------------------- */
