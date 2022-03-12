7eb051087ac9057553ddd26a443e2e125bf94475
Thuan Le <devicedev4@biotricity.com>
2022-03-12 20:46:32 +0700

    update: Setting LED successfull

--- main/main.ino
+++ main/main.ino
@@ -15,88 +15,49 @@
 #include "bsp_io_11.h"
 
 /* Private defines ---------------------------------------------------- */
-#define NUM_LED 10 // Tong so Led tren mach
+#define NUM_LED           (300) // Total LED of on strip
 
 /* Private enumerate/structure ---------------------------------------- */
 /* Private macros ----------------------------------------------------- */
 /* Public variables --------------------------------------------------- */
 /* Private variables -------------------------------------------------- */
-Adafruit_NeoPixel strip_0 = Adafruit_NeoPixel(NUM_LED, IO_LED_BUFFER_0, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_1 = Adafruit_NeoPixel(NUM_LED, IO_LED_BUFFER_1, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_2 = Adafruit_NeoPixel(NUM_LED, IO_LED_BUFFER_2, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_3 = Adafruit_NeoPixel(NUM_LED, IO_LED_BUFFER_3, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_4 = Adafruit_NeoPixel(NUM_LED, IO_LED_BUFFER_4, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_5 = Adafruit_NeoPixel(NUM_LED, IO_LED_BUFFER_5, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_6 = Adafruit_NeoPixel(NUM_LED, IO_LED_BUFFER_6, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_7 = Adafruit_NeoPixel(NUM_LED, IO_LED_BUFFER_7, NEO_GRB + NEO_KHZ800);
-
-/* Private function prototypes ---------------------------------------- */
-void rainbow_cycle(uint8_t wait);
+Adafruit_NeoPixel strip[8] = { Adafruit_NeoPixel() };
 
 /* Function definitions ----------------------------------------------- */
 void setup()
 {
-    strip_0.begin();
-    strip_1.begin();
-    strip_2.begin();
-    strip_3.begin();
-    strip_4.begin();
-    strip_5.begin();
-    strip_6.begin();
-    strip_7.begin();
+  strip[0].setPin(IO_LED_BUFFER_0);
+  strip[1].setPin(IO_LED_BUFFER_1);
+  strip[2].setPin(IO_LED_BUFFER_2);
+  strip[3].setPin(IO_LED_BUFFER_3);
+  strip[4].setPin(IO_LED_BUFFER_4);
+  strip[5].setPin(IO_LED_BUFFER_5);
+  strip[6].setPin(IO_LED_BUFFER_6);
+  strip[7].setPin(IO_LED_BUFFER_7);
 
-    strip_0.setBrightness(30);
-    strip_1.setBrightness(30);
-    strip_2.setBrightness(30);
-    strip_3.setBrightness(30);
-    strip_4.setBrightness(30);
-    strip_5.setBrightness(30);
-    strip_6.setBrightness(30);
-    strip_7.setBrightness(30);
-
-    strip_0.Color(255, 255, 255);
-    strip_1.Color(255, 255, 255);
-    strip_2.Color(255, 255, 255);
-    strip_3.Color(255, 255, 255);
-    strip_4.Color(255, 255, 255);
-    strip_5.Color(255, 255, 255);
-    strip_6.Color(255, 255, 255);
-    strip_7.Color(255, 255, 255);
-
-    strip_0.show();
-    strip_1.show();
-    strip_2.show();
-    strip_3.show();
-    strip_4.show();
-    strip_5.show();
-    strip_6.show();
-    strip_7.show();
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    strip[i].updateLength(NUM_LED);
+    strip[i].updateType(NEO_GRB + NEO_KHZ800);
+    strip[i].begin();
+    strip[i].setBrightness(30);
+  }
 }
 
 void loop()
 {
   uint16_t i;
 
-  for (i = 0; i < strip_0.numPixels(); i++)
+  for (uint8_t num_strip = 0; num_strip < 8; num_strip++)
   {
-    strip_0.setPixelColor(i, 200, 250, 0, 0);
-    strip_1.setPixelColor(i, 200, 250, 0, 0);
-    strip_2.setPixelColor(i, 200, 250, 0, 0);
-    strip_3.setPixelColor(i, 200, 250, 0, 0);
-    strip_4.setPixelColor(i, 200, 250, 0, 0);
-    strip_5.setPixelColor(i, 200, 250, 0, 0);
-    strip_6.setPixelColor(i, 200, 250, 0, 0);
-    strip_7.setPixelColor(i, 200, 250, 0, 0);
+    for (i = 0; i < strip[0].numPixels(); i++)
+    {
+      strip[num_strip].setPixelColor(i, 200, 250, 0, 0);
+    }
+
+    strip[num_strip].show();
   }
 
-  strip_0.show();
-  strip_1.show();
-  strip_2.show();
-  strip_3.show();
-  strip_4.show();
-  strip_5.show();
-  strip_6.show();
-  strip_7.show();
 }
 
 /* Private function definitions --------------------------------------- */
